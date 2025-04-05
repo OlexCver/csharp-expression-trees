24fbed9f42e83c1b7731daf0b77f56ddbe9166e5
Oleksandr Tsvirchkov <Oleksandr.tsvirchkov@3Shape.com>
2025-04-05 20:40:59 +0300

    The main code

--- TitanicExplorer/Pages/Index.cshtml.cs
+++ TitanicExplorer/Pages/Index.cshtml.cs
@@ -52,6 +52,7 @@ public class IndexModel : PageModel
     {
         Expression? currentExpression = null;
 
+        //The main code of th leacture  
         if (!string.IsNullOrEmpty(this.query))
         {
             var config = new ParsingConfig()
